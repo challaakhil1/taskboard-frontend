c4cf87fe48bb30400fe8d5228949466891adab98
Akhil Challa <challaakhil1@gmail.com>
2025-09-14 01:58:32 -0700

    Updated Docker

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5050/api"
+//   ,
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) req.headers.Authorization = `Bearer ${token}`;
+//   return req;
+// });
+
+// export default API;
+
+// src/axios.js
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5050/api",
+const instance = axios.create({
+  baseURL: `http://${window.location.hostname}:5050/api`, // ✅ resolves correctly even in Docker
+  withCredentials: true, // ✅ needed for cookies/JWT
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
-});
-
-export default API;
+export default instance;
